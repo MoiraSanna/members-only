3bb6b86f9f27a3c0ebcc50baed22cedf5ff68a22
moira <moira.sanna@gmail.com>
2019-03-17 12:13:22 +0000

    Implemented basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		log_in @user
   		flash[:success] = "Welcome, you are one of us now!"
   		redirect_to @user
   	else
